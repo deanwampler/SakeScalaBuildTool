c9f588b0f8f16b4b9a5e21557fead4c868002bf9
Dean Wampler <coaches@objectmentor.com>
2009-03-03 09:43:40 -0600

    Updated spec lib and made CLASSPATH definition more robust. Fixed warning in Result.scala.

--- bin/specrunner.scala
+++ bin/specrunner.scala
@@ -3,7 +3,7 @@ import org.specs.runner._
 /** 
  * Simple script to run the specs in the "spec" directory from a command line.
  * Run using:
- *   scala -cp lib/specs-1.4.1.jar:lib/junit-4.4.jar:build bin/specrunner.scala
+ *   scala -cp lib/specs-1.4.3.jar:lib/junit-4.4.jar:build bin/specrunner.scala
  */
 val runner = new SpecsFileRunner("spec", ".*")
 runner.report(runner.specs)
